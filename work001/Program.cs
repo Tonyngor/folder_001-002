8992984b81666a2169670b5dfb22962f97e1c0cf
Tonyngor <123629176+Tonyngor@users.noreply.github.com>
2023-03-16 05:48:09 +0300

    task 64, 66

---
+++ work001/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 64:
+// Задайте значение N. Напишите программу, которая выведет все натуральные числа
+// в промежутке от N до 1. Выполнить с помощью рекурсии.
+void printNumbers()
+{
+    Console.WriteLine("Задайте натуральное число N: ");
+    int numberN = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Натуральные числа от N до 1:");
+    while (numberN > 0)
+    {
+        Console.Write($"{numberN}, ");
+        numberN--;
+    }
+}
+printNumbers();
+
---
+++ work002/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+//  натуральных элементов в промежутке от M до N.
+
+void printSum()
+{
+    Console.WriteLine("Задайте натуральные числа M и N: ");
+    Console.WriteLine("Введите число M:");
+    int numberM = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число N:");
+    int numberN = Convert.ToInt32(Console.ReadLine());
+    int sum = 0;
+    if (numberN < numberM)
+    {
+        while (numberM +1 > numberN)
+        {
+            sum = sum + numberM;
+            numberM--;
+        }
+    }
+    else
+    {
+        while (numberM < numberN + 1)
+        {
+            sum = sum + numberM;
+            numberM++;
+        }
+    }
+    Console.Write($"сумма натуральных элементов в промежутке от M до N: {sum}");
+}
+printSum();
